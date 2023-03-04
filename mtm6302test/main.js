e6a171e16811b66cf304f2035d2cbfdf874d4a9d
song0129 <song0129@algonquinlive.com>
2023-03-04 15:14:46 -0500

    sorting fruit excercise

--- mtm6302test/main.js
+++ mtm6302test/main.js
@@ -12,4 +12,23 @@ let randomIndexTwo = Math.floor(Math.random () * fighters.length)
 stageEl.textContent = fighters[randomIndexOne] + ' vs ' + fighters[randomIndexTwo]
 
 })
-//console.log();
+
+//sorting fruit excercise
+let fruit = ['🍎', '🍊', '🍎', '🍎', '🍊']
+let appleShelf = document.getElementById('apple-shelf')
+let orangeShelf = document.getElementById('orange-shelf')
+
+//use loop, conditional statement,nand textContent put fruit on their respective shelf
+
+function sortFruit() {
+    for (let i = 0; i< fruit.length; i++){
+        if (fruit[i] === '🍎') {
+            appleShelf.textContent += '🍎'
+
+        } else if (fruit[i] ==='🍊') {
+            orangeShelf.textContent += '🍊'
+        }
+    }
+}
+
+sortFruit()
